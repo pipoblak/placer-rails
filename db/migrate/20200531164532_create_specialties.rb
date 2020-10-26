7d884cc5d4523cf353dff507cdd3cb9a116516a7
Felipe Ramos da Silva <pipoblakgamer@gmail.com>
2020-10-26 13:26:32 -0300

    Removing Specialty

--- db/migrate/20200531164532_create_specialties.rb
+++
@@ -1,9 +0,0 @@
-class CreateSpecialties < ActiveRecord::Migration[6.0]
-  def change
-    create_table :specialties, id: :uuid  do |t|
-      t.string :title
-
-      t.timestamps
-    end
-  end
-end
