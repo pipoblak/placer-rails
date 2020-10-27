d230a01fc651339988e9ef5d08aefd1187de182b
Felipe Ramos da Silva <pipoblakgamer@gmail.com>
2020-10-27 16:11:16 -0300

    Update Dealer Auth

--- app/controllers/api/v1/dealers/base_controller.rb
+++ app/controllers/api/v1/dealers/base_controller.rb
@@ -1,4 +1,4 @@
 class Api::V1::Dealers::BaseController < ApplicationController
   include DeviseTokenAuth::Concerns::SetUserByToken
-  before_action :authenticate_admin!
+  before_action :authenticate_dealer!
 end
